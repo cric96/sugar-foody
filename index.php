fe1a6b6cead7be7e31bdb9f46aec46cf5711a1fd
Chiara Forresi <forresi.chiara@gmail.com>
2017-11-24 22:24:09 +0100

    TODO remove  col-md-4 margin, signin e dati personali ok anche mobile

--- index.php
+++ index.php
@@ -2,18 +2,26 @@
 <html lang="it">
    <head>
       <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Benvenuto in Foody</title>
       <link rel="stylesheet" type="text/css" title="stylesheet" href="./css/main-style.css">
       <link href="http://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css" rel="stylesheet">
       <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
       <script src="http://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/js/bootstrap.js"></script>
+      <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet"/>
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
    </head>
    <body>
       <section class="main-block">
          <div class="container">
             <div class="row">
-               <div class="col-md-4 main-sec">
+               <div class=" col-md-4 main-sec">
                   <img src="./img/logo.png" alt="Foody" style="width:40%">
                   <h2 class="text-center">Login</h2>
                   <form method="post" class="login-form">
@@ -33,10 +41,10 @@
                   </form>
                </div>
                <div class="col-md-8 banner-sec">
-                  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                  <div id="foodslider" class="carousel slide" data-ride="carousel">
                      <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#foodslider" data-slide-to="0" class="active"></li>
+                        <li data-target="#foodslider" data-slide-to="1"></li>
                      </ol>
                      <div class="carousel-inner" role="listbox">
                         <div class="carousel-item active">
@@ -51,5 +59,7 @@
             </div>
          </div>
       </section>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
    </body>
 </html>
