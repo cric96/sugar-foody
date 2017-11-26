f13f7d1b1004eb41807024befc6e682f9caa34ae
cric96 <gianluca.aguzzi@studio.unibo.it>
2017-11-26 18:56:27 +0100

    aggiunta icona alla scheda

--- ordini.php
+++ ordini.php
@@ -2,6 +2,8 @@
 <html lang="it">
    <head>
       <meta charset="UTF-8">
+
+      <link rel="SHORTCUT ICON" href="img/logo.ico" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" type="text/css" title="stylesheet" href="./css/main-style.css">
