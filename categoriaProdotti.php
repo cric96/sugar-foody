81a2d1fe3ef7beee39e30e57eff709af6aa81d81
Luffarelli Marta! <marta.luffarelli@studio.unibo.it>
2017-11-26 18:22:38 +0100

    'Scelta categoria' ended, also with style

--- categoriaProdotti.php
+++ categoriaProdotti.php
@@ -4,16 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <script src="https://use.fontawesome.com/42b65516fc.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/catProdotti.css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
   <title>Categoria Prodotti</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">Benvenuto username</a>
+        <a class="img-item navbar-left" href="#">
+          <img src="./img/logo.png" alt="Logo">
+        </a>
+        <h1 class="navbar-brand">
+          BENVENUTO USERNAME
+        </h1>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -38,50 +45,58 @@
       </div>
     </nav>
       <!-- Page Content -->
-    <div role="main" class="container">
-
+    <header>
+      <div class="overlay">
+        <h1 class="my-4">Categorie</h1>
+      </div>
+    </header>
+    <div class="container content">
       <!-- Introduction Row -->
-      <h1 class="my-4">Scegli Categoria</h1>
       <!-- Team Members Row -->
       <div class="row">
-        <div class="col-lg-12">
-          <h2 class="my-4">Cosa desideri mangiare?</h2>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="Pasta">
+        <div class="col-lg-3 col-4 col-md-4 col-sm-4 text-center mb-2">
+          <img class="img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="Pasta">
           <h3>Pasta
           </h3>
         </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="Pizza">
+        <div class="col-lg-3 col-4 col-md-4 col-sm-4 text-center mb-2">
+          <img class="img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="Pizza">
           <h3>Pizza
           </h3>
         </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="Carne">
+        <div class="col-lg-3 col-4 col-md-4 col-sm-4 text-center mb-2">
+          <img class="img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="Carne">
           <h3>Secondo di carne
           </h3>
         </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="Pesce">
+        <div class="col-lg-3 col-4 col-md-4 col-sm-4 text-center mb-2">
+          <img class="img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="Pesce">
           <h3>Secondo di pesce
           </h3>
         </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="Fast food">
+        <div class="col-lg-3 col-4 col-md-4 col-sm-4 text-center mb-2">
+          <img class="img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="Fast food">
           <h3>Fast food
           </h3>
         </div>
-        <div class="col-lg-4 col-sm-6 text-center mb-4">
-          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="Piadine e crescioni">
+        <div class="col-lg-3 col-4 col-md-4 col-sm-4 text-center mb-2">
+          <img class="img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="Piadine e crescioni">
           <h3>Piadineria
           </h3>
         </div>
       </div>
-      <div class="Notifications">
-        <input type="button" name="Notification" value="Notification">
+      <div class="notification">
+        <a href="#">
+          <span class="fa fa-bell brown"></span>
+        </a>
       </div>
     </div>
     <!-- /.container -->
+    <footer class="panel-footer">
+      <a href="#" >Informativa privacy</a>
+      <a href="#" >Cookie</a>
+      <a href="#" >Help</a>
+      <a href="#" >FAQ</a>
+    </footer>
   </body>
 </html>
