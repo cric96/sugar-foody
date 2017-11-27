af975b840f7f303eb565b00fa69f35708bea6fd5
cric96 <gianluca.aguzzi@studio.unibo.it>
2017-11-27 11:39:48 +0100

    prima versione di listino non ancora finita

--- listino_admin.php
+++ listino_admin.php
@@ -9,12 +9,15 @@
      <script src="https://use.fontawesome.com/42b65516fc.js"></script>
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
      <link rel="stylesheet" href="./css/catProdotti.css">
+     <link rel="stylesheet" href="css/fattori_admin.css">
      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+     <script src="./js/hide-accessibily.js"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
      <title>Gestione ordine </title>
    </head>
    <body>
+
      <nav class="navbar navbar-expand-lg navbar-light bg fixed-top">
        <div class="container">
          <a class="img-item navbar-left" href="#">
@@ -46,13 +49,15 @@
          </div>
        </div>
      </nav>
-      <main>
-        <header>
-          <h1> Listino</h1>
-        </header>
+     <header>
+       <div class="overlay">
+         <h2 class="my-4">Listino</h2>
+       </div>
+     </header>
+      <main class="container">
         <section>
-          <table>
-            <thead>
+          <table class="table table-striped">
+            <thead thead-inverse>
               <tr>
                 <th>Nome prodotto</th>
                 <th>Prezzo prodotto</th>
@@ -61,25 +66,45 @@
               </tr>
             </thead>
             <tbody>
-              <tr>
+              <tr scope="row">
                 <td>Piada piadosa</td>
-                <td>100€<a href="#">+</a> </td>
-                <td></td>
-                <td></td>
+                <td>100€</a> </td>
+                <td class="modify"><a class="fa fa-cogs" href=#> <span class="hide-acc">modifica</span> </a></td>
+                <td class="delete"><a class="fa fa-trash" aria-hidden="true" href=#> <span class="hide-acc"> modifica</span> </a></td>
+              </tr>
+              <tr scope="row">
+                <td>Pizza pizzosa</td>
+                <td>100€</a> </td>
+                <td class="modify"><a class="fa fa-cogs" href=#> <span class="hide-acc">modifica</span> </a></td>
+                <td class="delete"><a class="fa fa-trash" aria-hidden="true" href=#><span class="hide-acc"> modifica</span></a></td>
+              </tr>
+              <tr scope="row">
+                <td>Pasta pastosa</td>
+                <td>100€</a> </td>
+                <td class="modify"><a class="fa fa-cogs" href=#><span class="hide-acc">modifica</span></a></td>
+                <td class="delete"><a class="fa fa-trash" aria-hidden="true" href=#> <span class="hide-acc"> modifica</span> </a></td>
+              </tr>
+              <tr scope="row">
+                <td>Dolce dolcioso</td>
+                <td>100€</a> </td>
+                <td class="modify"><a class="fa fa-cogs" href=#><span class="hide-acc">modifica</span></a></td>
+                <td class="delete"><a class="fa fa-trash" aria-hidden="true" href=#> <span class="hide-acc"> modifica</span> </a></td>
+              </tr>
+
+              <tr scope="row">
+                <td>Bibita zuccherosa</td>
+                <td>100€</a> </td>
+                <td class="modify"><a class="fa fa-cogs" href=#><span class="hide-acc">modifica</span></a></td>
+                <td class="delete"><a class="fa fa-trash" aria-hidden="true" href=#> <span class="hide-acc"> modifica</span> </a></td>
               </tr>
             </tbody>
           </table>
         </section>
         <div class="adder">
-          <a href="aggiungi_prodotto.php">+</a>
+          <a data-toggle="tooltip" title="Aggiungi prodotto!" class="fa fa-plus-square" aria-hidden="true" href="aggiungi_prodotto.php"><span class="hide-acc">+</span></a>
         </div>
+
       </main>
-      <div class="notification">
-        <input type="button" name="notifiche" value="notifiche">
-      </div>
-      <div class="goback">
-        <a href="home_admin.php"> Torna indietro.. </a>
-      </div>
       <div class="notification">
         <a href="#">
           <span class="fa fa-bell brown"></span>
