2d31040398166a7fb109612e54a9e55d06536bb6
Luffarelli Marta! <marta.luffarelli@studio.unibo.it>
2017-11-27 22:22:45 +0100

    some trouble with chrome caching

--- sceltaRistorante.php
+++ sceltaRistorante.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <script src="https://use.fontawesome.com/42b65516fc.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/catProdotti.css">
+    <link rel="stylesheet" href="./css/sceltaRistorante.css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
@@ -19,7 +19,7 @@
         <a class="navbar-left padded" href="#">
           <span class="fa fa-chevron-circle-left colored"></span>
         </a>
-        <a class="img-item navbar-left padded" href="#">
+        <a class="img-item navbar-left" href="#">
           <img src="./img/logo.png" alt="Logo">
         </a>
         <h1 class="navbar-brand">
