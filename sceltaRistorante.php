9a1f3c7284847f0f18fb60fbf98f391a376244e9
Chiara Forresi <forresi.chiara@gmail.com>
2017-11-29 15:46:43 +0100

    solve problem of freccettina

--- sceltaRistorante.php
+++ sceltaRistorante.php
@@ -7,15 +7,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <script src="https://use.fontawesome.com/42b65516fc.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-    <script src="https://www.w3schools.com/lib/w3.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="https://www.w3schools.com/lib/w3.js"></script>
     <link rel="stylesheet" href="./css/sceltaRistorante.css">
     <title>Scegli ristorante</title>
   </head>
   <body>
-    <div w3-include-html="navbarUtente.html"></div>
+    <div w3-include-html="navbarUtente2.html"></div>
     <header>
       <div class="overlay">
         <h2 class="my-4">Ristoranti</h2>
@@ -31,7 +31,12 @@
           </h3>
         </div>
       </div>
-      <div w3-include-html="notification.html"></div>
+    <div class="notification">
+      <a href="#">
+        <span class="fa fa-bell brown"></span>
+      </a>
+      <span class="badge badge-danger">2</span>
+    </div>
   </div>
   <div w3-include-html="footer.html"></div>
   <script>
