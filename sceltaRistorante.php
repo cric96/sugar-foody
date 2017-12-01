5c7145ff4bb1587a1b2fa6601137ee63f58c80d7
Luffarelli Marta! <marta.luffarelli@studio.unibo.it>
2017-12-01 20:16:17 +0100

    refactoring css files

--- sceltaRistorante.php
+++ sceltaRistorante.php
@@ -11,6 +11,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     <script src="https://www.w3schools.com/lib/w3.js"></script>
+    <link rel="stylesheet" href="./css/catProdotti.css">
+    <link rel="stylesheet" href="./css/componiOrdine.css">
     <link rel="stylesheet" href="./css/sceltaRistorante.css">
     <title>Scegli ristorante</title>
   </head>
