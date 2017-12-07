c6e85c5a7326b970d6e7e8c6617f54badd7abf9f
Chiara Forresi <forresi.chiara@gmail.com>
2017-12-07 20:49:55 +0100

    refactoring home_fattorino and adding fattorino navbar

--- home_fattorini.php
+++ home_fattorini.php
@@ -6,11 +6,13 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
       <script src="https://use.fontawesome.com/42b65516fc.js"></script>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://www.w3schools.com/lib/w3.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+      <script src="./js/scriptHide.js"></script>
       <script src="./js/hide-accessibily.js"></script>
       <link rel="stylesheet" href="./css/catProdotti.css">
       <link rel="stylesheet" href="./css/fa-style.css">
@@ -21,7 +23,7 @@
       <title>Ordini assegnati</title>
    </head>
    <body>
-      <nav w3-include-html="./include/navbarUtente.html" class="navbar navbar-expand-lg navbar-light bg fixed-top"></nav>
+      <nav w3-include-html="./include/navbarFattorino.html" class="navbar navbar-expand-lg navbar-light bg fixed-top"></nav>
       <header>
          <div class="overlay">
             <h2 class="my-4">Ordini assegnati</h2>
@@ -29,11 +31,11 @@
       </header>
       <main class="container">
          <section>
-            <h3 class="hide-acc">Ordini a te assegnati</h3>
+            <h3 class="hide-acc">Storico ordini</h3>
             <table class="table table-striped">
                <thead>
                   <tr>
-                     <th>Numero ordine</th>
+                     <th>#ordine</th>
                      <th>Stato</th>
                      <th>Luogo</th>
                      <th>Data / Ora</th>
@@ -42,26 +44,26 @@
                </thead>
                <tbody>
                   <tr>
-                     <td>Numero ordine</td>
+                     <td>1234</td>
                      <td>
                        <select name="status" class="selectpicker showtick">
-                          <option value="assegnato">Assegnato</option>
-                          <option value="preso">Preso in carico</option>
-                          <option value="elaborazione">Elaborazione</option>
-                          <option value="consegna">In consegna</option>
-                          <option value="concluso">Concluso</option>
-                        </select>
+                        <option value="assegnato">Assegnato</option>
+                        <option value="preso">Preso in carico</option>
+                        <option value="elaborazione">Elaborazione</option>
+                        <option value="consegna">In consegna</option>
+                        <option value="concluso">Concluso</option>
+                      </select>
                      </td>
                      <td>Luogo</td>
                      <td>Data</td>
-                     <td class="info"><a class="fa fa-info fa-2x" href=dettagli_ordine.php><span class="hide-acc">Dettagli</span></a></td>
+                     <td class="info"><a class="fa fa-info fa-2x" aria-hidden="true" data-toggle="modal" data-target="#myModal" href=dettagli_ordine.php><span class="hide-acc">Dettagli</span></a></td>
                   </tr>
                   <tr>
-                     <td>Numero ordine</td>
+                     <td>45343</td>
                      <td>Stato</td>
                      <td>Luogo</td>
                      <td>Data</td>
-                     <td class="info"><a class="fa fa-info fa-2x" aria-hidden="true" href=dettagli_ordine.php><span class="hide-acc">Dettagli</span></a></td>
+                     <td class="info"><a class="fa fa-info fa-2x" data-toggle="modal" data-target="#myModal" aria-hidden="true" href=dettagli_ordine.php><span class="hide-acc">Dettagli</span></a></td>
                   </tr>
                </tbody>
             </table>
@@ -69,6 +71,47 @@
       </main>
       <div w3-include-html="./include/notification.html" class="notification"></div>
       <footer class="panel-footer" w3-include-html="./include/footer.html"></footer>
+      <!-- The Modal -->
+      <div class="modal fade" id="myModal">
+         <div class="modal-dialog">
+            <div class="modal-content">
+               <!-- Modal Header -->
+               <div class="modal-header">
+                  <h4 class="modal-title">Ordine # <!--numero ordine in php--></h4>
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+               </div>
+               <!-- Modal body -->
+               <div class="modal-body">
+                 <table class="table table-striped">
+                    <thead>
+                       <tr>
+                          <th>Prodotto</th>
+                          <th>Quantità</th>
+                          <th>Categoria</th>
+                          <th>Prezzo</th>
+                       </tr>
+                    </thead>
+                    <tbody>
+                       <tr>
+                          <td>Pizza</td>
+                          <td>Quantità</td>
+                          <td>Categoria</td>
+                          <td>Prezzo</td>
+                       </tr>
+                       <tr>
+                          <td>Piadina</td>
+                          <td>Quantità</td>
+                          <td>Categoria</td>
+                          <td>Prezzo</td>
+                       </tr>
+                    </tbody>
+                 </table>
+               </div>
+               <!-- Modal footer <div class="modal-footer">
+                  </div> -->
+            </div>
+         </div>
+      </div>
       <script>
          w3.includeHTML();
       </script>
