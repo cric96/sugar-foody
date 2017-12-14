8155eae5b9d213fabb1d9bcdb16c822b62223669
Luffarelli Marta! <marta.luffarelli@studio.unibo.it>
2017-12-14 16:09:37 +0100

    modify something in componiOrdine

--- js/hidinPayments.js
+++ js/hidinPayments.js
@@ -1,12 +1,14 @@
 function valueChanged()
 {
    if($('.check-hide').is(":checked")) {
+     $(".buttons-hides").hide();
      $(".fieldset-hide").show(1000);
      $('#nomeTitolare').attr('required', true);
      $('#cognomeTitolare').attr('required', true);
      $('#psw').attr('required', true);
      $('#date').attr('required', true);
    } else {
+     $(".buttons-hides").show();
      $(".fieldset-hide").hide(1000);
      $('#nomeTitolare').removeAttr('required');
      $('#cognomeTitolare').removeAttr('required');
