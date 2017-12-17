45b81790a180f913e9e5e99527fda85c3f88fe13
Luffarelli Marta! <marta.luffarelli@studio.unibo.it>
2017-12-17 22:19:24 +0100

    modify conn to cn in config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 	$cn = new mysqli('localhost', 'root', '', 'my_sugarfoody');
 	/* check connection */
 	if ($cn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
+	    die("Connection failed: " . $cn->connect_error);
 	}
 ?>
