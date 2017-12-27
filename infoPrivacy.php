681988a846aafda3584c9380ce3873980ae26606
Luffarelli Marta! <marta.luffarelli@studio.unibo.it>
2017-12-27 22:37:27 +0100

    workin on infoPrivacy

---
+++ infoPrivacy.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="SHORTCUT ICON" href="img/logo.ico" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <script src="https://use.fontawesome.com/42b65516fc.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="https://www.w3schools.com/lib/w3.js"></script>
+    <script src="./js/hide-accessibily.js"></script>
+    <link rel="stylesheet" href="./css/infoPrivacy.css">
+    <title>Privacy</title>
+  </head>
+  <body>
+    <section>
+      <h2>Privacy policy per i visitatori/utenti del sito</h2>
+      <p>La presente Privacy Policy ha lo scopo di descrivere le modalità di gestione di questo sito, in
+        riferimento al trattamento dei dati personali degli utenti/visitatori che lo consultano. Si tratta di
+        un’informativa che è resa anche ai sensi dell’art. 13 del D. Lgs. 196/03 - Codice in materia di
+        protezione dei dati personali – a coloro che si collegano al sito web www.sugar-foody.it
+        e usufruiscono dei relativi servizi web. L’informativa è resa soltanto per i siti sopra menzionati e non anche
+        per altri siti web eventualmente consultati dall’utente tramite appositi link.</p>
+        <p>Il sito è di proprietà di CGM , gestito da CGM che
+          garantisce il rispetto della normativa in materia di protezione dei dati personali (D. Lgs. 196/03).
+          Gli utenti/visitatori dovranno leggere attentamente la presente Privacy Policy prima di inoltrare
+          qualsiasi tipo di informazione personale e/o compilare qualunque modulo elettronico presente sul
+          sito stesso.</p>
+    </section>
+    <section>
+      <h2>Tipologia dei dati trattati e finalità del trattamento</h2>
+      <ol>
+        <li>
+          <h3>Dati di navigazione</h3>
+          <p>I sistemi informatici e le procedure software preposte al funzionamento di questo sito
+             acquisiscono, nel normale esercizio, alcuni dati personali che vengono poi trasmessi
+             implicitamente nell’uso dei protocolli di comunicazione Internet. Si tratta di informazioni
+             che non sono raccolte per essere associate a interessati identificati, ma che per loro
+             stessa natura potrebbero, attraverso elaborazioni e associazioni con dati detenuti da
+             terzi, permettere di identificare gli utenti. In questa categoria di dati rientrano gli indirizzi
+             IP o i nomi a dominio dei computer utilizzati dagli utenti che si connettono al sito, gli
+             indirizzi in notazione URI (Uniform Resource Identifier) delle risorse richieste, l’orario
+             della richiesta, il metodo utilizzato nel sottoporre la richiesta al server, la dimensione del
+             file ottenuto in risposta, il codice numerico indicante lo stato della risposta data dal
+             server (buon fine, errore, ecc.) ed altri parametri relativi al sistema operativo e
+             all’ambiente informatico dell’utente. Questi dati vengono utilizzati al solo fine di ricavare
+             informazioni statistiche anonime sull’uso del sito e per controllarne il corretto
+             funzionamento e vengono cancellati immediatamente dopo l’elaborazione. I dati
+             potrebbero essere utilizzati per l’accertamento di responsabilità in caso di ipotetici reati
+             informatici ai danni del sito: salva questa eventualità.
+         </p>
+        </li>
+        <li>
+          <h3>Dati forniti volontariamente dagli utenti/visitatori</h3>
+          <p>Qualora gli utenti/visitatori, collegandosi a questo sito, inviino propri dati personali per
+            accedere a determinati servizi, ovvero per effettuare richieste in posta elettronica, ciò
+            comporta l’acquisizione da parte di CGM dell’indirizzo del mittente e/o di altri
+            eventuali dati personali che verranno trattati esclusivamente per rispondere alla
+            richiesta, ovvero per la fornitura del servizio. I dati personali forniti dagli utenti/visitatori
+            verranno comunicati a terzi solo nel caso in cui la II comunicazione sia necessaria per
+            ottemperare alle richieste degli utenti/visitatori medesimi.
+         </p>
+        </li>
+      </ol>
+    </section>
+    <section>
+      <h2>Modalità del trattamento</h2>
+      <p>I dati personali sono trattati con strumenti automatizzati per il tempo strettamente
+        necessario a conseguire gli scopi per cui sono stati raccolti. Specifiche misure di
+        sicurezza sono osservate per prevenire la perdita dei dati, usi illeciti o non corretti ed
+        accessi non autorizzati. Facoltatività del conferimento dei dati A parte quanto
+        specificato per i dati di navigazione, gli utenti/visitatori sono liberi di fornire i propri dati
+        personali. Il loro mancato conferimento può comportare unicamente l’impossibilità di
+        ottenere quanto richiesto.</p>
+    </section>
+    <section>
+      <h2>Luogo di trattamento dei dati</h2>
+      <p>I trattamenti connessi ai servizi web dei siti sopra indicati, hanno luogo presso la sede aziendale
+        di CGM., in Viale dei Ciliegi 17 - 39122 Pillopallo del Titolare del trattamento e/o
+        presso la sede della società di hosting e/o gestione del sito web e sono curati solo da personale
+        tecnico dell’Ufficio incaricato del trattamento, oppure da eventuali incaricati di occasionali
+        operazioni di manutenzione. Nessun dato derivante dal servizio web viene comunicato o diffuso.
+        I dati personali forniti dagli utenti che inoltrano richieste di invio di materiale informativo sono
+        utilizzati al solo fine di eseguire il servizio o la prestazione richiesta e sono comunicati a terzi nel
+        solo caso in cui ciò sia a tal fine necessario</p>
+    </section>
+    <section>
+      <h2>Titolare del trattamento</h2>
+      <p>Il titolare del trattamento dei dati personali è il sig. Cricchio De Criccis</p>
+    </section>
+    <section>
+      <h2>Diritti degli interessati</h2>
+      <p>I soggetti cui si riferiscono i dati personali hanno il diritto in qualunque momento di
+        ottenere la III conferma dell’esistenza o meno dei medesimi dati e di conoscerne il
+        contenuto e l’origine, verificarne l’esattezza o chiederne l’integrazione l’aggiornamento,
+        oppure la rettificazione (art. 7 del D. Lgs. 196/03). Ai sensi del medesimo articolo si ha il
+        diritto di chiedere la cancellazione, la trasformazione in forma anonima o il blocco dei
+        dati trattati in violazione di legge, nonché di opporsi in ogni caso, per motivi legittimi, al
+        loro trattamento. Le richieste vanno inoltrate al Titolare del trattamento ai recapiti
+        indicati sopra.</p>
+    </section>
+  </body>
+</html>
