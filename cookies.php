b42e694b1117a1d22b96a5cfbadc89616c5bfa4f
Luffarelli Marta! <marta.luffarelli@studio.unibo.it>
2017-12-28 13:27:43 +0100

    workin on footer

---
+++ cookies.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="SHORTCUT ICON" href="img/logo.ico" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <script src="https://use.fontawesome.com/42b65516fc.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="https://www.w3schools.com/lib/w3.js"></script>
+    <script src="./js/hide-accessibily.js"></script>
+    <link rel="stylesheet" href="./css/infoPrivacy.css">
+    <title>Cookie</title>
+  </head>
+  <body>
+    <section id="cookie">
+      <h2>Cookies</h2>
+      <h3>Cosa sono i cookie tecnici</h3>
+      <p>I cookie tecnici sono quelli utilizzati al solo fine di “effettuare la trasmissione di una comunicazione
+        su una rete di comunicazione elettronica, o nella misura strettamente necessaria al fornitore di un servizio
+        della società dell’informazione esplicitamente richiesto dall’abbonato o dall’utente a erogare tale servizio”
+        (cfr. art. 122, comma 1, del Codice).
+        Essi non vengono utilizzati per scopi ulteriori e sono normalmente installati direttamente dal titolare o
+        gestore del sito web. Possono essere suddivisi in cookie di navigazione o di sessione, che garantiscono la
+        normale navigazione e fruizione del sito web (permettendo, ad esempio, di realizzare un acquisto o
+        autenticarsi per accedere ad aree riservate); cookie analytics, assimilati ai cookie tecnici laddove
+        utilizzati direttamente dal gestore del sito per raccogliere informazioni, in forma aggregata, sul numero
+        degli utenti e su come questi visitano il sito stesso; cookie di funzionalità, che permettono all’utente la
+        navigazione in funzione di una serie di criteri selezionati (ad esempio, la lingua, i prodotti selezionati
+        per l’acquisto) al fine di migliorare il servizio reso allo stesso.
+        Per l’installazione di tali cookie non è richiesto il preventivo consenso degli utenti, mentre resta fermo
+        l’obbligo di dare l’informativa ai sensi dell’art. 13 del Codice, che il gestore del sito, qualora utilizzi
+        soltanto tali dispositivi, potrà fornire con le modalità che ritiene più idonee.”</p>
+      <h3>Cookie utilizzati nel sito</h3>
+      <p>Non viene fatto uso di cookies per la trasmissione di informazioni di carattere personale,
+          né vengono utilizzati c.d. cookies persistenti di alcun tipo, ovvero sistemi per il
+          tracciamento degli utenti. L’uso di c.d. cookies di sessione (che non vengono
+          memorizzati in modo persistente sul computer dell’utente e svaniscono con la chiusura
+          del browser) è strettamente limitato alla trasmissione di identificativi di sessione
+          (costituiti da numeri casuali generati dal server) necessari per consentire l’esplorazione
+          sicura ed efficiente del sito. I c.d. cookies di sessione utilizzati in questo sito evitano il
+          ricorso ad altre tecniche informatiche potenzialmente pregiudizievoli per la riservatezza
+          della navigazione degli utenti e non consentono l’acquisizione di dati personali
+          identificativi dell’utente.</p>
+    </section>
+  </body>
+</html>
---
+++ faq.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="SHORTCUT ICON" href="img/logo.ico" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <script src="https://use.fontawesome.com/42b65516fc.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="https://www.w3schools.com/lib/w3.js"></script>
+    <script src="./js/hide-accessibily.js"></script>
+    <link rel="stylesheet" href="./css/infoPrivacy.css">
+    <title>FAQ</title>
+  </head>
+  <body>
+    <section>
+      <h2>Domande frequenti: cliente</h2>
+      <h3>Come posso ordinare?</h3>
+      <p>
+        Una volta esserti registrato ed aver effettuato il login con le tue credenziali, potrai effettuare
+        un ordine scegliendo, tra la lista di ristoranti che ti troverai davanti, quello da cui vuoi ordinare.
+        <span id="attention">Ricorda</span>, puoi ordinare da un solo ristorante per ordine.
+      </p>
+      <h3>Una volta scelto il ristornate come procedo?</h3>
+      <p>Non appena avrai scelto il ristorante da cui ordinare, verrai indirizzato ad una pagina
+        che ti mostrerà quali sono le categorie di prodotti che puoi prendere presso quel fornitore.
+        E' possibile ordinare diverse categorie di prodotti in uno stesso ordine, purché
+        siano dello stesso ristorante.
+      </p>
+      <h3>Come capisco se un prodotto è stato aggiunto al mio carrello?</h3>
+      <p>Una volta scelto il ristorante e la categoria di prodotto da ordinare, ti verrà mostrata una
+        lista di prodotti disponibili presso quel fornitore. Una volta scelto il prodotto, per aggiungerlo
+        al carrello ti basterà cliccare sul carrellino sulla linea del prodotto da te scelto e successivamente
+        sul tasto "conferma" a destra della finestra che ti comparirà.
+      </p>
+    </section>
+    <section>
+      <h2>Domande frequenti: admin</h2>
+      <h3>Come posso ordinare?</h3>
+      <p>
+        Una volta esserti registrato ed aver effettuato il login con le tue credenziali, potrai effettuare
+        un ordine scegliendo, tra la lista di ristoranti che ti troverai davanti, quello da cui vuoi ordinare.
+        <span id="attention">Ricorda</span>, puoi ordinare da un solo ristorante per ordine.
+      </p>
+      <h3>Una volta scelto il ristornate come procedo?</h3>
+      <p>Non appena avrai scelto il ristorante da cui ordinare, verrai indirizzato ad una pagina
+        che ti mostrerà quali sono le categorie di prodotti che puoi prendere presso quel fornitore.
+        E' possibile ordinare diverse categorie di prodotti in uno stesso ordine, purché
+        siano dello stesso ristorante.
+      </p>
+      <h3>Come capisco se un prodotto è stato aggiunto al mio carrello?</h3>
+      <p>Una volta scelto il ristorante e la categoria di prodotto da ordinare, ti verrà mostrata una
+        lista di prodotti disponibili presso quel fornitore. Una volta scelto il prodotto, per aggiungerlo
+        al carrello ti basterà cliccare sul carrellino sulla linea del prodotto da te scelto e successivamente
+        sul tasto "conferma" a destra della finestra che ti comparirà.
+      </p>
+    </section>
+    <section>
+      <h2>Domande frequenti: fattorino</h2>
+      <h3>Come posso ordinare?</h3>
+      <p>
+        Una volta esserti registrato ed aver effettuato il login con le tue credenziali, potrai effettuare
+        un ordine scegliendo, tra la lista di ristoranti che ti troverai davanti, quello da cui vuoi ordinare.
+        <span id="attention">Ricorda</span>, puoi ordinare da un solo ristorante per ordine.
+      </p>
+      <h3>Una volta scelto il ristornate come procedo?</h3>
+      <p>Non appena avrai scelto il ristorante da cui ordinare, verrai indirizzato ad una pagina
+        che ti mostrerà quali sono le categorie di prodotti che puoi prendere presso quel fornitore.
+        E' possibile ordinare diverse categorie di prodotti in uno stesso ordine, purché
+        siano dello stesso ristorante.
+      </p>
+      <h3>Come capisco se un prodotto è stato aggiunto al mio carrello?</h3>
+      <p>Una volta scelto il ristorante e la categoria di prodotto da ordinare, ti verrà mostrata una
+        lista di prodotti disponibili presso quel fornitore. Una volta scelto il prodotto, per aggiungerlo
+        al carrello ti basterà cliccare sul carrellino sulla linea del prodotto da te scelto e successivamente
+        sul tasto "conferma" a destra della finestra che ti comparirà.
+      </p>
+    </section>
+  </body>
+</html>
