2765a333ecf9ee5915c7c50fdc285a034db3eaaf
Luffarelli Marta! <marta.luffarelli@studio.unibo.it>
2017-12-30 10:22:31 +0100

    workin on footer

--- cookies.php
+++ cookies.php
@@ -16,35 +16,38 @@
     <title>Cookie</title>
   </head>
   <body>
-    <section id="cookie">
-      <h2>Cookies</h2>
-      <h3>Cosa sono i cookie tecnici</h3>
-      <p>I cookie tecnici sono quelli utilizzati al solo fine di “effettuare la trasmissione di una comunicazione
-        su una rete di comunicazione elettronica, o nella misura strettamente necessaria al fornitore di un servizio
-        della società dell’informazione esplicitamente richiesto dall’abbonato o dall’utente a erogare tale servizio”
-        (cfr. art. 122, comma 1, del Codice).
-        Essi non vengono utilizzati per scopi ulteriori e sono normalmente installati direttamente dal titolare o
-        gestore del sito web. Possono essere suddivisi in cookie di navigazione o di sessione, che garantiscono la
-        normale navigazione e fruizione del sito web (permettendo, ad esempio, di realizzare un acquisto o
-        autenticarsi per accedere ad aree riservate); cookie analytics, assimilati ai cookie tecnici laddove
-        utilizzati direttamente dal gestore del sito per raccogliere informazioni, in forma aggregata, sul numero
-        degli utenti e su come questi visitano il sito stesso; cookie di funzionalità, che permettono all’utente la
-        navigazione in funzione di una serie di criteri selezionati (ad esempio, la lingua, i prodotti selezionati
-        per l’acquisto) al fine di migliorare il servizio reso allo stesso.
-        Per l’installazione di tali cookie non è richiesto il preventivo consenso degli utenti, mentre resta fermo
-        l’obbligo di dare l’informativa ai sensi dell’art. 13 del Codice, che il gestore del sito, qualora utilizzi
-        soltanto tali dispositivi, potrà fornire con le modalità che ritiene più idonee.”</p>
-      <h3>Cookie utilizzati nel sito</h3>
-      <p>Non viene fatto uso di cookies per la trasmissione di informazioni di carattere personale,
-          né vengono utilizzati c.d. cookies persistenti di alcun tipo, ovvero sistemi per il
-          tracciamento degli utenti. L’uso di c.d. cookies di sessione (che non vengono
-          memorizzati in modo persistente sul computer dell’utente e svaniscono con la chiusura
-          del browser) è strettamente limitato alla trasmissione di identificativi di sessione
-          (costituiti da numeri casuali generati dal server) necessari per consentire l’esplorazione
-          sicura ed efficiente del sito. I c.d. cookies di sessione utilizzati in questo sito evitano il
-          ricorso ad altre tecniche informatiche potenzialmente pregiudizievoli per la riservatezza
-          della navigazione degli utenti e non consentono l’acquisizione di dati personali
-          identificativi dell’utente.</p>
-    </section>
+    <img src="./img/logo.png" alt="Logo del sito">
+    <main>
+      <section id="cookie">
+        <h2>Cookies</h2>
+        <h3>Cosa sono i cookie tecnici</h3>
+        <p>I cookie tecnici sono quelli utilizzati al solo fine di “effettuare la trasmissione di una comunicazione
+          su una rete di comunicazione elettronica, o nella misura strettamente necessaria al fornitore di un servizio
+          della società dell’informazione esplicitamente richiesto dall’abbonato o dall’utente a erogare tale servizio”
+          (cfr. art. 122, comma 1, del Codice).
+          Essi non vengono utilizzati per scopi ulteriori e sono normalmente installati direttamente dal titolare o
+          gestore del sito web. Possono essere suddivisi in cookie di navigazione o di sessione, che garantiscono la
+          normale navigazione e fruizione del sito web (permettendo, ad esempio, di realizzare un acquisto o
+          autenticarsi per accedere ad aree riservate); cookie analytics, assimilati ai cookie tecnici laddove
+          utilizzati direttamente dal gestore del sito per raccogliere informazioni, in forma aggregata, sul numero
+          degli utenti e su come questi visitano il sito stesso; cookie di funzionalità, che permettono all’utente la
+          navigazione in funzione di una serie di criteri selezionati (ad esempio, la lingua, i prodotti selezionati
+          per l’acquisto) al fine di migliorare il servizio reso allo stesso.
+          Per l’installazione di tali cookie non è richiesto il preventivo consenso degli utenti, mentre resta fermo
+          l’obbligo di dare l’informativa ai sensi dell’art. 13 del Codice, che il gestore del sito, qualora utilizzi
+          soltanto tali dispositivi, potrà fornire con le modalità che ritiene più idonee.”</p>
+        <h3>Cookie utilizzati nel sito</h3>
+        <p>Non viene fatto uso di cookies per la trasmissione di informazioni di carattere personale,
+            né vengono utilizzati c.d. cookies persistenti di alcun tipo, ovvero sistemi per il
+            tracciamento degli utenti. L’uso di c.d. cookies di sessione (che non vengono
+            memorizzati in modo persistente sul computer dell’utente e svaniscono con la chiusura
+            del browser) è strettamente limitato alla trasmissione di identificativi di sessione
+            (costituiti da numeri casuali generati dal server) necessari per consentire l’esplorazione
+            sicura ed efficiente del sito. I c.d. cookies di sessione utilizzati in questo sito evitano il
+            ricorso ad altre tecniche informatiche potenzialmente pregiudizievoli per la riservatezza
+            della navigazione degli utenti e non consentono l’acquisizione di dati personali
+            identificativi dell’utente.</p>
+      </section>
+    </main>
   </body>
 </html>
--- faq.php
+++ faq.php
@@ -16,60 +16,63 @@
     <title>FAQ</title>
   </head>
   <body>
-    <section>
-      <h2>Domande frequenti: cliente</h2>
-      <h3>Come posso ordinare?</h3>
-      <p>
-        Una volta esserti registrato ed aver effettuato il login con le tue credenziali, potrai effettuare
-        un ordine scegliendo, tra la lista di ristoranti che ti troverai davanti, quello da cui vuoi ordinare.
-        <span id="attention">Ricorda</span>, puoi ordinare da un solo ristorante per ordine.
-      </p>
-      <h3>Una volta scelto il ristornate come procedo?</h3>
-      <p>Non appena avrai scelto il ristorante da cui ordinare, verrai indirizzato ad una pagina
-        che ti mostrerà quali sono le categorie di prodotti che puoi prendere presso quel fornitore.
-        E' possibile ordinare diverse categorie di prodotti in uno stesso ordine, purché
-        siano dello stesso ristorante.
-      </p>
-      <h3>Come capisco se un prodotto è stato aggiunto al mio carrello?</h3>
-      <p>Una volta scelto il ristorante e la categoria di prodotto da ordinare, ti verrà mostrata una
-        lista di prodotti disponibili presso quel fornitore. Una volta scelto il prodotto, per aggiungerlo
-        al carrello ti basterà cliccare sul carrellino sulla linea del prodotto da te scelto e successivamente
-        sul tasto "conferma" a destra della finestra che ti comparirà.
-      </p>
-    </section>
-    <section>
-      <h2>Domande frequenti: admin</h2>
-      <h3>Come assegno un ordine a un fattorino?</h3>
-      <p>
-        Nella sezione ordini è presente un + per assegnare l'ordine al fattorino.
-      </p>
-      <h3>Ho un nuovo fattorino, posso registrarlo?</h3>
-      <p>Dalla pagina fattorini può registrare un nuovo fattorino, modificarlo o eliminarlo.
-         Le credenziali devono essere assegnate da lei, per poi essere modificate dal fattorino dopo aver
-         effettuato l'accesso.
-      </p>
-      <h3>Un mio cliente / fattorino ha perso le sue credenziali, cosa posso fare?</h3>
-      <p>Contattaci, risolveremo assieme il problema.
-      </p>
-      <h3>Come capisco se un prodotto è stato al mio listino?</h3>
-      <p>Il prodotto sarà presente nella pagina listino.
-      </p>
-    </section>
-    <section>
-      <h2>Domande frequenti: fattorino</h2>
-      <h3>Come posso cambiare la mia password?</h3>
-      <p>
-        Dopo che il tuo datore di lavoro ti ha fornito le tue credenziali, dopo aver effettuato l'accesso,
-        potrai modificare i tuoi dati personali dalla voce presente nel menù.
-        <span id="attention">Ricorda</span> la tua nuova password.
-      </p>
-      <h3>Come modifico lo stato di un ordine?</h3>
-      <p>Non appena avrai effettuato l'accesso potrai vedere tutti gli ordini a te assegnati, cliccando sullo stato
-      potrai assegnare all'ordine ad esso relativo quello specifico stato (stessa cosa per l'effettuazione del pagamento).
-      </p>
-      <h3>Dove vedo il totale dell'ordine da far pagare al cliente?</h3>
-      <p>Cliccando nelle info dell'ordine ci sarà tutto ciò che il cliente ha ordinato e anche il totale da pagare.
-      </p>
-    </section>
+    <img src="./img/logo.png" alt="Logo del sito">
+    <main>
+      <section>
+        <h2>Domande frequenti: cliente</h2>
+        <h3>Come posso ordinare?</h3>
+        <p>
+          Una volta esserti registrato ed aver effettuato il login con le tue credenziali, potrai effettuare
+          un ordine scegliendo, tra la lista di ristoranti che ti troverai davanti, quello da cui vuoi ordinare.
+          <span id="attention">Ricorda</span>, puoi ordinare da un solo ristorante per ordine.
+        </p>
+        <h3>Una volta scelto il ristornate come procedo?</h3>
+        <p>Non appena avrai scelto il ristorante da cui ordinare, verrai indirizzato ad una pagina
+          che ti mostrerà quali sono le categorie di prodotti che puoi prendere presso quel fornitore.
+          E' possibile ordinare diverse categorie di prodotti in uno stesso ordine, purché
+          siano dello stesso ristorante.
+        </p>
+        <h3>Come capisco se un prodotto è stato aggiunto al mio carrello?</h3>
+        <p>Una volta scelto il ristorante e la categoria di prodotto da ordinare, ti verrà mostrata una
+          lista di prodotti disponibili presso quel fornitore. Una volta scelto il prodotto, per aggiungerlo
+          al carrello ti basterà cliccare sul carrellino sulla linea del prodotto da te scelto e successivamente
+          sul tasto "conferma" a destra della finestra che ti comparirà.
+        </p>
+      </section>
+      <section>
+        <h2>Domande frequenti: admin</h2>
+        <h3>Come assegno un ordine a un fattorino?</h3>
+        <p>
+          Nella sezione ordini è presente un + per assegnare l'ordine al fattorino.
+        </p>
+        <h3>Ho un nuovo fattorino, posso registrarlo?</h3>
+        <p>Dalla pagina fattorini può registrare un nuovo fattorino, modificarlo o eliminarlo.
+           Le credenziali devono essere assegnate da lei, per poi essere modificate dal fattorino dopo aver
+           effettuato l'accesso.
+        </p>
+        <h3>Un mio cliente / fattorino ha perso le sue credenziali, cosa posso fare?</h3>
+        <p>Contattaci, risolveremo assieme il problema.
+        </p>
+        <h3>Come capisco se un prodotto è stato al mio listino?</h3>
+        <p>Il prodotto sarà presente nella pagina listino.
+        </p>
+      </section>
+      <section>
+        <h2>Domande frequenti: fattorino</h2>
+        <h3>Come posso cambiare la mia password?</h3>
+        <p>
+          Dopo che il tuo datore di lavoro ti ha fornito le tue credenziali, dopo aver effettuato l'accesso,
+          potrai modificare i tuoi dati personali dalla voce presente nel menù.
+          <span id="attention">Ricorda</span> la tua nuova password.
+        </p>
+        <h3>Come modifico lo stato di un ordine?</h3>
+        <p>Non appena avrai effettuato l'accesso potrai vedere tutti gli ordini a te assegnati, cliccando sullo stato
+        potrai assegnare all'ordine ad esso relativo quello specifico stato (stessa cosa per l'effettuazione del pagamento).
+        </p>
+        <h3>Dove vedo il totale dell'ordine da far pagare al cliente?</h3>
+        <p>Cliccando nelle info dell'ordine ci sarà tutto ciò che il cliente ha ordinato e anche il totale da pagare.
+        </p>
+      </section>
+    </main>
   </body>
 </html>
